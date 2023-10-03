48a2c9583df9f0dd7343447a42cd89dcb8fe10ff
Herrera Daniel <danielherrera2009@hotmail.es>
2023-10-03 17:07:14 -0300

    testing y final

--- api/src/main/java/med/voll/api/domain/consulta/DatosAgendarConsulta.java
+++ api/src/main/java/med/voll/api/domain/consulta/DatosAgendarConsulta.java
@@ -8,14 +8,13 @@ import med.voll.api.domain.medico.Especialidad;
 import java.time.LocalDateTime;
 
 public record DatosAgendarConsulta(
-        Long id,
         @NotNull
         Long idPaciente,
         Long idMedico,
-        Especialidad especialidad,
         @NotNull
         @Future // Future para que la fecha indicada sea posterior a la actual
         @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
-        LocalDateTime fecha
+        LocalDateTime fecha,
+        Especialidad especialidad
 ) {
 }
